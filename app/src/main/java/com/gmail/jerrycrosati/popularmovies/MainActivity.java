d73dd9a084d920ddd544a49c5dfb61237cbb5137
geraldrosati <jerry.c.rosati@gmail.com>
2017-02-09 13:31:39 -0500

    Added permissions and layout items

--- app/src/main/java/com/gmail/jerrycrosati/popularmovies/MainActivity.java
+++ app/src/main/java/com/gmail/jerrycrosati/popularmovies/MainActivity.java
@@ -2,6 +2,12 @@ package com.gmail.jerrycrosati.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
 }
---
+++ app/src/main/java/com/gmail/jerrycrosati/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,8 @@
+package com.gmail.jerrycrosati.popularmovies.utilities;
+
+import android.net.Uri;
+import java.net.URL;
+
+public class NetworkUtils {
+
+}
