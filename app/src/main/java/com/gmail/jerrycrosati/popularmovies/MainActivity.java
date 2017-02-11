66af4b8e882cc8fe56fbc82d2cc1a6aa21f7a198
Jerry <jerry.c.rosati@gmail.com>
2017-02-10 22:07:28 -0500

    Added moviedb query

--- app/src/main/java/com/gmail/jerrycrosati/popularmovies/MainActivity.java
+++ app/src/main/java/com/gmail/jerrycrosati/popularmovies/MainActivity.java
@@ -1,20 +1,84 @@
 package com.gmail.jerrycrosati.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.gmail.jerrycrosati.popularmovies.utilities.NetworkUtils;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+    TextView _jsonDisplay;
+    TextView _errorMessageDisplay;
+    ProgressBar _loadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _jsonDisplay = (TextView) findViewById(R.id.tv_movie_json);
+        _errorMessageDisplay = (TextView) findViewById(R.id.tv_error_message);
+        _loadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        makeMovieDatabaseQuery();
+    }
+
+    private void makeMovieDatabaseQuery() {
+        URL movieDatabaseUrl = NetworkUtils.buildUrl("popular");
+        new MovieDatabaseQueryTask().execute(movieDatabaseUrl);
+    }
+
+    private void showJsonDataView() {
+        _errorMessageDisplay.setVisibility(View.INVISIBLE);
+        _jsonDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        _errorMessageDisplay.setVisibility(View.VISIBLE);
+        _jsonDisplay.setVisibility(View.INVISIBLE);
+    }
+
+    public class MovieDatabaseQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            _loadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String movieDatabaseResults = null;
+            try {
+                movieDatabaseResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return movieDatabaseResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieDatabaseResults) {
+            _loadingIndicator.setVisibility(View.INVISIBLE);
+
+            if ((movieDatabaseResults != null) && (movieDatabaseResults.equals("") == false)) {
+                _jsonDisplay.setText(movieDatabaseResults);
+            } else {
+                showErrorMessage();
+            }
+        }
     }
 
     @Override
@@ -25,6 +89,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return true;
+        int clickedItemId = item.getItemId();
+        if (clickedItemId == R.id.refresh) {
+            makeMovieDatabaseQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/gmail/jerrycrosati/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/gmail/jerrycrosati/popularmovies/utilities/NetworkUtils.java
@@ -1,8 +1,51 @@
 package com.gmail.jerrycrosati.popularmovies.utilities;
 
 import android.net.Uri;
+import android.net.wifi.ScanResult;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class NetworkUtils {
+    private static final String MOVIE_DB_BASE_URL = "http://api.themoviedb.org/3/movie/";
+    private static final String MOVIE_DB_API_KEY = "";
+    private static final String API_KEY = "api_key";
 
+    public static URL buildUrl(String sortMethod) {
+        Uri builtUri = Uri.parse(MOVIE_DB_BASE_URL).buildUpon()
+                .appendPath(sortMethod)
+                .appendQueryParameter(API_KEY, MOVIE_DB_API_KEY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
