a841db7574c2e391f87f7fc5fc47b91cfbf9ed39
Jerry Rosati <jerry.c.rosati@gmail.com>
2017-05-21 13:36:53 -0400

    Changed dimensions and text sizes on the details activity

--- app/src/main/java/com/gmail/jerrycrosati/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/gmail/jerrycrosati/popularmovies/DetailsActivity.java
@@ -2,7 +2,6 @@ package com.gmail.jerrycrosati.popularmovies;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
